e672143c5eb5002e713d9edb1f35fe1f8d6b3d1c
O-Gui <guilherme.basilio2.0@gmail.com>
2025-04-09 10:16:52 -0300

    att

--- Loja.py
+++ Loja.py
@@ -1,4 +1,13 @@
-class Loja:
-    def __init__(self, comprar):
-        self.comprar = comprar
+from Carrinho import Carrinho
+from Produto import Produto
 
+class Loja:
+    def __init__(self):
+        self.carrinho = Carrinho()
+        self.produtos = [
+            Produto("celular", 1000.0, 10),
+            Produto("notebook", 1500.0, 5),
+            Produto("PC gamer", 2000.0, 11),
+            Produto("teclado", 150.0, 8),     
+            Produto("mouse", 100.0, 10)
+        ]
--- Produto.py
+++ Produto.py
@@ -1,5 +1,8 @@
 class Produto:
-    def __init__(self, produto, preco):
-        self.name = produto
-        self.age = preco
-    
+    def __init__(self, nome, preco, estoque):
+        self.nome = nome
+        self.preco = preco
+        self.estoque = estoque
+
+    def desconto(self, percentual):
+        return self.preco * (1 - percentual / 100)
